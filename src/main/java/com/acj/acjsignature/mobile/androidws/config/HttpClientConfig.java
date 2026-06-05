427d46cc67cf64628230c581836f64fb4c337b85
MatFragg <u202318323@upc.edu.pe>
2026-06-04 22:28:32 -0500

    fix(deps): correct bucket4j artifactId and resttemplate config

--- src/main/java/com/acj/acjsignature/mobile/androidws/config/HttpClientConfig.java
+++ src/main/java/com/acj/acjsignature/mobile/androidws/config/HttpClientConfig.java
@@ -1,8 +1,8 @@
 package com.acj.acjsignature.mobile.androidws.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -11,10 +11,10 @@ import java.time.Duration;
 public class HttpClientConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-            .setConnectTimeout(Duration.ofSeconds(5))
-            .setReadTimeout(Duration.ofSeconds(5))
-            .build();
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(5).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(5).toMillis());
+        return new RestTemplate(factory);
     }
 }
